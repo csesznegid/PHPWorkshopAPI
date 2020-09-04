255bc5f3b49e6ff682cff7ab4d8a30d370bdeec4
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-04 15:45:12 +0200

    chore: add main router and service manager

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$service = (array_key_exists('service', $_REQUEST) ? $_REQUEST['service'] : '');
+try {
+    require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'autoloader.php');
+    \Framework\Logger\Logger::Access('Access to ' . $service);
+
+    $controllerClass = ('\App\Controller\\' . $service . 'Controller');
+    $controllerObj   = new $controllerClass();
+    echo($controllerObj->run());
+    \Framework\Logger\Logger::Success('Successfully called ' . $service);
+}
+catch(\Exception $e) {
+    $errorMsg = ('Failed to call ' . $service . ' - ' . $e->getMessage());
+    \Framework\Logger\Logger::Error($errorMsg);
+    echo(\Framework\Helper\JSON::Encode($errorMsg));
+}
